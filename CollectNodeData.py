55acb75ef9897ea74b1eb7af6f5970061d745106
lascavana <lascavuzzo@gmail.com>
2021-12-21 12:57:04 +0100

    catch node solved

--- CollectNodeData.py
+++ CollectNodeData.py
@@ -1,5 +1,4 @@
-from pyscipopt import scip
-import pyscipopt as pyscipopt
+import pyscipopt
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 import joblib
@@ -15,199 +14,196 @@ class EventHandler(pyscipopt.Eventhdlr):
         self.stats = {}
 
     def eventinit(self):
-        self.model.catchEvent(pyscipopt.SCIP_EVENTTYPE.NODEFOCUSED, self)
-        self.model.catchEvent(pyscipopt.SCIP_EVENTTYPE.NODEFEASIBLE, self)
-        #self.model.catchEvent(pyscipopt.SCIP_EVENTTYPE.NODEINFEASIBLE, self)
-        #self.model.catchEvent(pyscipopt.SCIP_EVENTTYPE.NODEBRANCHED, self)
+        self.model.catchEvent(pyscipopt.SCIP_EVENTTYPE.NODESOLVED, self)
 
     def eventexit(self):
-        self.model.catchEvent(pyscipopt.SCIP_EVENTTYPE.NODEFOCUSED, self)
-        self.model.dropEvent(pyscipopt.SCIP_EVENTTYPE.NODEFEASIBLE, self)
-        #self.model.dropEvent(pyscipopt.SCIP_EVENTTYPE.NODEINFEASIBLE, self)
-        #self.model.dropEvent(pyscipopt.SCIP_EVENTTYPE.NODEBRANCHED, self)
+        self.model.dropEvent(pyscipopt.SCIP_EVENTTYPE.NODESOLVED, self)
 
     def eventexec(self, event):
 
         event_type = event.getType()
+        if (event_type == pyscipopt.SCIP_EVENTTYPE.NODEFEASIBLE):
+        	print('Node feasible')
+        if (event_type == pyscipopt.SCIP_EVENTTYPE.NODEINFEASIBLE):
+        	print('Node infeasible')
+        if (event_type == pyscipopt.SCIP_EVENTTYPE.NODEBRANCHED):
+        	print('Node branched')
 
-        if(event_type == pyscipopt.SCIP_EVENTTYPE.NODEFOCUSED):
+        depth = self.model.getDepth()
+        primal = self.model.getPrimalbound()
+        print('depth ', depth)
+        print("primal ", primal)
 
-            print("NODEFOCUSED,NODEFOCUSED,NODEFOCUSED,NODEFOCUSED,NODEFOCUSED,NODEFOCUSED")
-            depth = self.model.getDepth()
-            primal = self.model.getPrimalbound()
-            print('depth ', depth)
-            print("primal ", primal)
-
-            nodeID = self.model.getCurrentNode().getNumber()
-            nodeFeatures = []
-
-
-            # optimality gap
-            optimality_gap = self.model.getGap()
-            nodeFeatures.append(optimality_gap)
-
-            # infinity gap
-            isInf = self.model.isInfinity(optimality_gap)
-            nodeFeatures.append(isInf)
-
-            # Root LP value / Global Lower Bound
-            nodeFeatures.append(self.model.getDualboundRoot( ) /self.model.getLPObjVal())
-
-            # Root LP value / Global Upper Bound
-            nodeFeatures.append(self.model.getDualboundRoot( ) /(self.model.getLPObjVal( ) +optimality_gap))
-
-            # Node Depth
-            nodeFeatures.append(self.model.getDepth())
-
-            vars = self.model.getVars()
-            fracVars = []
-            nOfFrac = 0
-            fractionalities = []
-            for var in vars:
-                LPSol = var.getLPSol()
-                if abs(LPSol- int(LPSol)) < 0.00001:
-                    fracVars.append(var)
-                    nOfFrac += 1
-                    fractionalities.append(LPSol - np.floor(LPSol))
-
-            # Error occurs:LPCan = self.model.getLPBranchCands()
-            # fracVars = LPCan[0]
-            # fractionalities = LPCan[2]
-            # nOfFrac = LPCan[3]
-            nOfUpLocks = []
-            nOfDownLocks = []
-            distanceToLPSol = []
-            vectorLen = []
-            pseudocost = []
-            for var in fracVars:
-
-                nOfUpLocks.append(var.getNLocksUp())
-                nOfDownLocks.append(var.getNLocksDown())
-                distanceToLPSol.append(var.getLPSol( ) -self.model.getDualboundRoot())
-
-                #vector length
-                cost = var.getObj()
-                # col = var.getCol()
-                # temp = np.array(col.getVals())
-                # LPSol = var.getLPSol()
-                # frac = LPSol - np.floor(LPSol)
-                # if (cost > 0):
-                #     vec_length = ( 1 -frac) * cost / (np.linalg.norm(temp ) +1)
-                # else:
-                #     vec_length = - frac * cost / (np.linalg.norm(temp) + 1)
-                # vectorLen. append(vec_length)
-                vectorLen.append(0)
-                pseudocost.append(self.model.getVarPseudocost(var))
-
-            # Sum of variables’ LP solution fractionalities / Num. of Fractional Variables
-            if nOfFrac == 0:
-                nodeFeatures.append(1)
-            else:
-                nodeFeatures.append(np.sum(fractionalities) / nOfFrac)
-
-            # Num. of Fractional Variable / Num. of Integer Variables
-            if (len(vars) - nOfFrac) == 0:
-                nodeFeatures.append(1)
-            else:
-                nodeFeatures.append(nOfFrac / (len(vars) - nOfFrac))
-
-            # Num. of Active Constraints / Num. of Constraints
-            conss = self.model.getConss()
-            nOfActiveCons = 0
-            for cons in conss:
-                if(cons.isActive()):
-                    nOfActiveCons += 1
-            nodeFeatures.append(nOfActiveCons/ len(conss))
-
-            # Node is root
-            if(nodeID == 1):
-                nodeFeatures.append(True)
-            else:
-                nodeFeatures.append(False)
-
-            # Root LP value / Node LB
-            nodeFeatures.append(self.model.getDualboundRoot() / self.model.getCurrentNode().getLowerbound())
-            # Root LP value / Node Estimate
-
-            # Node estimate
-            estimate = self.model.getCurrentNode().getEstimate()
-            nodeFeatures.append(self.model.getDualboundRoot() / estimate)
-
-            # Number of Up Locks
-            if nOfFrac == 0:
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-            else:
-                nodeFeatures.append(np.mean(nOfUpLocks))
-                nodeFeatures.append(np.min(nOfUpLocks))
-                nodeFeatures.append(np.max(nOfUpLocks))
-                nodeFeatures.append(np.std(nOfUpLocks))
-                nodeFeatures.append(np.median(nOfUpLocks))
-
-            # Number of Down Locks
-            if nOfFrac == 0:
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-            else:
-                nodeFeatures.append(np.mean(nOfDownLocks))
-                nodeFeatures.append(np.min(nOfDownLocks))
-                nodeFeatures.append(np.max(nOfDownLocks))
-                nodeFeatures.append(np.std(nOfDownLocks))
-                nodeFeatures.append(np.median(nOfDownLocks))
-
-            # Normalized Objective Coefﬁcient
-
-            # Distance to root LP solution
-            if nOfFrac == 0:
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-            else:
-                nodeFeatures.append(np.mean(distanceToLPSol))
-                nodeFeatures.append(np.min(distanceToLPSol))
-                nodeFeatures.append(np.max(distanceToLPSol))
-                nodeFeatures.append(np.std(distanceToLPSol))
-                nodeFeatures.append(np.median(distanceToLPSol))
-
-            # Vector Length
-            if nOfFrac == 0:
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-            else:
-                nodeFeatures.append(np.mean(vectorLen))
-                nodeFeatures.append(np.min(vectorLen))
-                nodeFeatures.append(np.max(vectorLen))
-                nodeFeatures.append(np.std(vectorLen))
-                nodeFeatures.append(np.median(vectorLen))
-
-            # Pseudocost score
-            if nOfFrac == 0:
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-                nodeFeatures.append(0)
-            else:
-                nodeFeatures.append(np.mean(pseudocost))
-                nodeFeatures.append(np.min(pseudocost))
-                nodeFeatures.append(np.max(pseudocost))
-                nodeFeatures.append(np.std(pseudocost))
-                nodeFeatures.append(np.median(pseudocost))
+        nodeID = self.model.getCurrentNode().getNumber()
+        nodeFeatures = []
 
 
-            self.stats[nodeID] = {}
-            self.stats[nodeID]['features'] = nodeFeatures
+        # optimality gap
+        optimality_gap = self.model.getGap()
+        nodeFeatures.append(optimality_gap)
+
+        # infinity gap
+        isInf = self.model.isInfinity(optimality_gap)
+        nodeFeatures.append(isInf)
+
+        # Root LP value / Global Lower Bound
+        nodeFeatures.append(self.model.getDualboundRoot( ) /self.model.getLPObjVal())
+
+        # Root LP value / Global Upper Bound
+        nodeFeatures.append(self.model.getDualboundRoot( ) /(self.model.getLPObjVal( ) +optimality_gap))
+
+        # Node Depth
+        nodeFeatures.append(self.model.getDepth())
+
+        vars = self.model.getVars()
+        fracVars = []
+        nOfFrac = 0
+        fractionalities = []
+        for var in vars:
+            LPSol = var.getLPSol()
+            if abs(LPSol- int(LPSol)) < 0.00001:
+                fracVars.append(var)
+                nOfFrac += 1
+                fractionalities.append(LPSol - np.floor(LPSol))
+
+        # Error occurs:LPCan = self.model.getLPBranchCands()
+        # fracVars = LPCan[0]
+        # fractionalities = LPCan[2]
+        # nOfFrac = LPCan[3]
+        nOfUpLocks = []
+        nOfDownLocks = []
+        distanceToLPSol = []
+        vectorLen = []
+        pseudocost = []
+        for var in fracVars:
+
+            nOfUpLocks.append(var.getNLocksUp())
+            nOfDownLocks.append(var.getNLocksDown())
+            distanceToLPSol.append(var.getLPSol( ) -self.model.getDualboundRoot())
+
+            #vector length
+            cost = var.getObj()
+            # col = var.getCol()
+            # temp = np.array(col.getVals())
+            # LPSol = var.getLPSol()
+            # frac = LPSol - np.floor(LPSol)
+            # if (cost > 0):
+            #     vec_length = ( 1 -frac) * cost / (np.linalg.norm(temp ) +1)
+            # else:
+            #     vec_length = - frac * cost / (np.linalg.norm(temp) + 1)
+            # vectorLen. append(vec_length)
+            vectorLen.append(0)
+            pseudocost.append(self.model.getVarPseudocost(var))
+
+        # Sum of variables’ LP solution fractionalities / Num. of Fractional Variables
+        if nOfFrac == 0:
+            nodeFeatures.append(1)
+        else:
+            nodeFeatures.append(np.sum(fractionalities) / nOfFrac)
+
+        # Num. of Fractional Variable / Num. of Integer Variables
+        if (len(vars) - nOfFrac) == 0:
+            nodeFeatures.append(1)
+        else:
+            nodeFeatures.append(nOfFrac / (len(vars) - nOfFrac))
+
+        # Num. of Active Constraints / Num. of Constraints
+        conss = self.model.getConss()
+        nOfActiveCons = 0
+        for cons in conss:
+            if(cons.isActive()):
+                nOfActiveCons += 1
+        nodeFeatures.append(nOfActiveCons/ len(conss))
+
+        # Node is root
+        if(nodeID == 1):
+            nodeFeatures.append(True)
+        else:
+            nodeFeatures.append(False)
+
+        # Root LP value / Node LB
+        nodeFeatures.append(self.model.getDualboundRoot() / self.model.getCurrentNode().getLowerbound())
+        # Root LP value / Node Estimate
+
+        # Node estimate
+        estimate = self.model.getCurrentNode().getEstimate()
+        nodeFeatures.append(self.model.getDualboundRoot() / estimate)
+
+        # Number of Up Locks
+        if nOfFrac == 0:
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+        else:
+            nodeFeatures.append(np.mean(nOfUpLocks))
+            nodeFeatures.append(np.min(nOfUpLocks))
+            nodeFeatures.append(np.max(nOfUpLocks))
+            nodeFeatures.append(np.std(nOfUpLocks))
+            nodeFeatures.append(np.median(nOfUpLocks))
+
+        # Number of Down Locks
+        if nOfFrac == 0:
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+        else:
+            nodeFeatures.append(np.mean(nOfDownLocks))
+            nodeFeatures.append(np.min(nOfDownLocks))
+            nodeFeatures.append(np.max(nOfDownLocks))
+            nodeFeatures.append(np.std(nOfDownLocks))
+            nodeFeatures.append(np.median(nOfDownLocks))
+
+        # Normalized Objective Coefﬁcient
+
+        # Distance to root LP solution
+        if nOfFrac == 0:
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+        else:
+            nodeFeatures.append(np.mean(distanceToLPSol))
+            nodeFeatures.append(np.min(distanceToLPSol))
+            nodeFeatures.append(np.max(distanceToLPSol))
+            nodeFeatures.append(np.std(distanceToLPSol))
+            nodeFeatures.append(np.median(distanceToLPSol))
+
+        # Vector Length
+        if nOfFrac == 0:
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+        else:
+            nodeFeatures.append(np.mean(vectorLen))
+            nodeFeatures.append(np.min(vectorLen))
+            nodeFeatures.append(np.max(vectorLen))
+            nodeFeatures.append(np.std(vectorLen))
+            nodeFeatures.append(np.median(vectorLen))
+
+        # Pseudocost score
+        if nOfFrac == 0:
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+            nodeFeatures.append(0)
+        else:
+            nodeFeatures.append(np.mean(pseudocost))
+            nodeFeatures.append(np.min(pseudocost))
+            nodeFeatures.append(np.max(pseudocost))
+            nodeFeatures.append(np.std(pseudocost))
+            nodeFeatures.append(np.median(pseudocost))
+
+
+        self.stats[nodeID] = {}
+        self.stats[nodeID]['features'] = nodeFeatures
 
 
 
@@ -215,8 +211,9 @@ class EventHandler(pyscipopt.Eventhdlr):
 instance = "ins.lp"
 
 
-m = scip.Model()
+m = pyscipopt.Model()
 m.readParams("params.set")
+m.setParam('display/verblevel', 0)
 m.readProblem(instance)
 
 
@@ -314,5 +311,3 @@ def training():
     #loadedModel = joblib.load(fileName)
 
 training()
-
-
